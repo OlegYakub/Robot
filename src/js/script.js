85627df08ad7665a8dd676c84a390157bd761914
OlegYakub <zeiro299@mail.ru>
2016-11-17 14:42:47 +0200

    брокер и сделки

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,36 @@
-$('.post-slide').slick({
-  dots: false,
-  arrows: false,
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  speed: 300,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  centerPadding: '20px',
-  responsive: [
-    
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-   
-  ]
+ $( function() {
+    $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
 });
+
+  $('.post-slide').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerPadding: '20px',
+    responsive: [
+      
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+     
+    ]
+  });
+  $('.select').styler();  
+
